7db6a5e48989abaca6a250e30d1ec329531022a7
Matt Graham <matthewgraham@nhs.net>
2020-10-30 10:54:12 +0000

    removing trailing whitespace, formatting comments better

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ from task_list.tasks.TaskList import TaskList
 from task_list.users.User import User
 from task_list.users.Admin import Admin
 
-#instantiate an example task list
+# instantiate an example task list
 tasks = TaskList()
-#instantiate an example user
+# instantiate an example user
 regularUser = User("Regular", "User", 1234)
-#instantiate an example admin
+# instantiate an example admin
 adminUser = Admin("Admin", "User", 4246)
--- task_list/Audit.py
+++ task_list/Audit.py
@@ -1,10 +1,13 @@
 from datetime import datetime
 
-#simple class at the moment, but could be expanded to include other fields like "user"
+
+# simple class at the moment, but could be
+# expanded to include other fields like "user"
 class Audit:
+
     def __init__(self):
         self.timestamp = datetime.now()
-    
+
     def __str__(self):
         return "Timestamp: {0:%d/%m/%y %H:%M}".format(self.timestamp)
 
