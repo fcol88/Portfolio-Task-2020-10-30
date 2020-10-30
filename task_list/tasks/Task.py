308b1a8bac9001c81c5875b863a7cc6fb67a2309
Matt Graham <matthewgraham@nhs.net>
2020-10-30 11:09:12 +0000

    refactoring lines to be shorter, fixing formatting issues

--- task_list/tasks/Task.py
+++ task_list/tasks/Task.py
@@ -1,19 +1,20 @@
 from task_list.Audit import Audit
 
-#define Task class
+
+# define Task class
 class Task:
-    #initialise Task - uses datetime imported library
+    # initialise Task - uses datetime imported library
     def __init__(self, user, description):
         self.status = "To Do"
         self.created = Audit()
         self.completed = None
-        
+
         self.user = user
         self.description = description
 
-    #complete task - usually called from TaskList
+    # complete task - usually called from TaskList
     def completeTask(self):
-        #if the task hasn't already been marked as completed...
+        # if the task hasn't already been marked as completed...
         if self.status != "Done":
             # ...mark it as completed...
             self.status = "Done"
@@ -21,12 +22,22 @@ class Task:
             self.completed = Audit()
             # ...then return a string to be printed
             return "Task marked as completed"
-        #if the task has been completed, return a string to be printed
+        # if the task has been completed, return a string to be printed
         return "Task already completed!"
 
     # returns task as a string
     def __str__(self):
-        return "Task: Description={0}, Status={1}, User={2} {3} Created={4:%d/%m/%y %H:%M}".format(self.description, self.status, self.user.firstName, self.user.lastName, self.created.timestamp) + ("" if self.completed == None else " || Completed: {0:%d/%m/%y %H:%M}".format(self.completed.timestamp))
+
+        description = "Task: Description={0}".format(self.description)
+        status = ", Status={0}".format(self.status)
+        user = ", User={0} {1}".format(self.user.firstName, self.user.lastName)
+        created = ", Created={0:%d/%m/%y %H:%M}".format(self.created.timestamp)
+        completed = ""
+        if self.completed is not None:
+            completed = ", Completed: {0:%d/%m/%y %H:%M}".format(
+                self.completed.timestamp)
+
+        return description + status + user + created + completed
 
     # returns code used to instantiate object
     def __repr__(self):
